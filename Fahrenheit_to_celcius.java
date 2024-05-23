4119fa15b6bda2e60e347f093e5d60a9accff3d8
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2024-05-23 15:49:22 +0530

    Add files via upload

---
+++ Fahrenheit_to_celcius.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class Fahrenheit_to_celcius {
+
+	public static void main(String[] args) {
+		int fahrenheit1=100;
+		int celcius1;
+		System.out.println("Enter fahrenheit:"+fahrenheit1);
+		celcius1=(fahrenheit1-32)*5/9;
+		System.out.println("celcius1: "+celcius1);
+	}
+
+}
+//o/p:-------------
+//Enter fahrenheit:100
+//celcius1: 37
