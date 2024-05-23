6b05c29fe03a45ff013cbba64053cedac19120ba
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2024-05-23 15:43:49 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,35 @@
+package package1;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+				/* datatypes are 3 types PRIMITIVE,REFERENCE(class,int,enum array),NULL
+				 * PRIMITIVE===BOOLEAN AND NUMERIC
+				 * NUMERIC are  1) INTEGER(byte,short,int,long,char)
+				 *              2) FLOAT(float,double)
+				 * 
+				 */
+				byte b=1;
+				short s=2;
+				int x=5;
+				long l=234567;
+				char ch='p';
+				double d=234.43;
+				System.out.println("byte"+b);
+				System.out.println("short"+s);
+				System.out.println("integer"+x);
+				System.out.println("long"+l);
+				System.out.println("char"+ch);
+				System.out.println("double"+d);
+				
+			}
+	}
+
+//0/p:------
+//byte1
+//short2
+//integer5
+//long234567
+//charp
+//double234.43
