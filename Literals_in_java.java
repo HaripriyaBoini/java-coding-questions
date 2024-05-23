c0f033277e0dc9e2fe55a71bd38643b7637d3fb5
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2024-05-23 15:38:40 +0530

    Add files via upload

---
+++ Literals_in_java.java
@@ -0,0 +1,39 @@
+package package1;
+
+public class Literals_in_java {
+
+	public static void main(String[] args) {
+		int decliteral=100;
+	      System.out.println("decimal literal:" +decliteral);
+	int octalliteral=0146;
+	      System.out.println("octal literal:" +octalliteral);
+	int hexadecliteral=0xFF;
+	      System.out.println("hexadecliteral:" +hexadecliteral);
+	int binaryliteral=0111;
+	      System.out.println("binary literal:" +binaryliteral);
+	      System.out.println('\n');
+	//character integral
+	char singleQuote='A';
+			System.out.println("singleQuote" +singleQuote);
+	char integralLiteral=0101;
+	        System.out.println("integral literal" +integralLiteral);
+	char unicodeLiteral='\u0041';
+	        System.out.println("unicode Literal" +unicodeLiteral );
+	char escapeLiteral='\n';
+	        System.out.println("escape character"+escapeLiteral);
+	
+	        //other Literals
+	        //string Literals
+	        String name="unocareer";
+	        String age="25";
+	        boolean isPassed=true;
+	        boolean isopen=false;
+	        System.out.println("name:  " +name);
+	        System.out.println("age" +age);
+	        System.out.println("ispassed" +isPassed);
+	        System.out.println("isopen" +isopen);
+
+
+	}
+
+}
