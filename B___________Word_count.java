b7d17c3a48a40826aa3604ec806c5964a60ec452
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2024-06-03 17:24:48 +0530

    Add files via upload

---
+++ B___________Word_count.java
@@ -0,0 +1,35 @@
+package package1;
+
+import java.util.Scanner;
+
+public class B___________Word_count {
+	
+	static int wordcount(String str) {
+		int count=0;
+		char[] ch= new char[str.length()]; 
+		for(int i=0;i<str.length();i++) {
+			
+			ch[i]=str.charAt(i);
+			
+			 if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) ) 
+				count++;
+			
+		}return count;
+	}
+		
+	
+	public static void main(String[] args) { 
+		
+		Scanner sc =new Scanner(System.in);
+		String str=sc.nextLine();
+		sc.close();
+		System.out.println("No of words are:"
+		+wordcount(str));
+				
+	}
+		
+	}
+	
+	
+	
+
