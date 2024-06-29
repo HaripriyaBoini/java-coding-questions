d72e52bddb03baadbabd5f23fd95cfb1d184a202
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2024-06-29 14:53:22 +0530

    Add files via upload

---
+++ H_a_____________convert_int_to_String.java
@@ -0,0 +1,42 @@
+package package1;
+
+public class H_a_____________convert_int_to_String {
+/*
+ * public static void main(String[] args) {
+    DoNotTerminate.forbidExit();
+    try{
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        in.close();
+        
+        if(n==Integer.parseInt(s)){
+            System.out.println("Good job");
+        }else{
+            System.out.println("Wrong Answer");
+        }
+    }catch(DoNotTerminate.ExitTrappedException e){
+        System.out.println("Unsuccesful Termination");
+    }
+        
+    }
+}
+Class DoNotTerminate {
+    public static class ExitTrappedException extends SecurityException{
+        private static final long serialVersionUID=1;
+        
+    }
+    public static void forbitExit{
+        final SecurityManager securityManager=new SecurityManager(){
+            
+            public void checkPermission(Permission permission){
+                if(permission.getName().contains("exitVM")){
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+        
+    }
+ */
+
+}
