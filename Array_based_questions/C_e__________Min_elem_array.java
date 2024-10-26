17e8253a898d55f57abc54b2bb294ccb51b4be44
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2024-10-26 12:41:43 +0530

    Add files via upload

---
+++ Array_based_questions/C_e__________Min_elem_array.java
@@ -0,0 +1,18 @@
+package Array_based_questions;
+
+public class C_e__________Min_elem_array {
+
+	public static void main(String[] args) {
+		int[] num= {2,5,1,7,8,9};
+		int min_elem=num[0];
+		
+		for(int i=0;i<num.length;i++) {
+			if(num[i]<min_elem) {
+				min_elem=num[i];
+			}
+		}
+		System.out.println(" min_elem  "+min_elem);
+
+	}
+
+}
